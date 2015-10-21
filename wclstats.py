d3b76cfc7b32b439242d44fa89fa7a90ab19f9f0
cyfirgit <cyfir@cox.net>
2015-10-21 06:43:55 -0700

    Build 01
    Add Bootstrap and jQuery

--- wclstats.py
+++ wclstats.py
@@ -1,11 +1,18 @@
 # coding: utf-8
 
+import os
+import jinja2
 import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain; charset=UTF-8'
-        self.response.write("Hi!")
+		template = JINJA_ENVIRONMENT.get_template("templates/wclstats.html")
+		self.response.write(template.render({}))
 
 		
 app = webapp2.WSGIApplication([
