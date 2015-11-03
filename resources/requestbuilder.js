f125a9af54c919dc6a91935d4f455cc2984908f8
cyfirgit <cyfir@cox.net>
2015-11-02 23:19:17 -0700

    Build 07
    Added macros for templating
    Implemented function for iterating dynamic form id

--- resources/requestbuilder.js
+++ resources/requestbuilder.js
@@ -1,7 +1,25 @@
+var formID = [];
+
+function setID(idType, dimension, parameter, spell_id) {
+	if (idType === 'dimension') {
+		console.log('Adding index [' + dimension + ']');
+		formID[dimension] = [];
+	} else if (idType === 'parameter') {
+		console.log('Adding index [' + dimension + '][' + parameter + ']');
+		formID[dimension][parameter] = [];
+	} else if (idType === 'spell_id') {
+		if (spell_id >= 200) {
+		console.log('Adding index [' + dimension + '][' + parameter + '][2][' + spell_id + ']');
+			formID[dimension][parameter][2] = (spell_id - 200);
+		} else if (spell_id >= 100) {
+		console.log('Adding index [' + dimension + '][' + parameter + '][1][' + spell_id + ']');
+			formID[dimension][parameter][1] = (spell_id - 100);
+		};
+	};	
+};
+
 function selectRequest() {
 	var selectedRequest = this.id;
-	console.log("Firing selectRequest");
-	console.log(selectedRequest);
 	$( '#requestName' ).val(selectedRequest);
 	
 	var data = {
